a4f2d0e6e76de1e7ee85f72aaefceb0dc6b71903
tonyscott44 <acsii@me.com>
2018-07-10 23:32:41 -0400

    updated package.json in root

--- routes/api/index.js
+++ routes/api/index.js
@@ -8,21 +8,21 @@ const songRoutes = require("./songs");
 const userRoutes = require("./users");
 
 // User routes
-router.use("/users", userRoutes);
-
-// Profile routes
-router.use("/profiles", profileRoutes);
-
-// Friend routes
-router.use("/friends", friendRoutes);
-
-// Playlist routes
-router.use("/playlists", playlistRoutes);
-
-// Song routes
-router.use("/songs", songRoutes);
-
-// Like routes
-router.use("/likes", likeRoutes);
+// router.use("/users", userRoutes);
+//
+// // Profile routes
+// router.use("/profiles", profileRoutes);
+//
+// // Friend routes
+// router.use("/friends", friendRoutes);
+//
+// // Playlist routes
+// router.use("/playlists", playlistRoutes);
+//
+// // Song routes
+// router.use("/songs", songRoutes);
+//
+// // Like routes
+// router.use("/likes", likeRoutes);
 
 module.exports = router;
